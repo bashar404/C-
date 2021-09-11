6a341d54445de2f94bc19daaf3aa639fa77c9a24
Golam Dastoger Bashar <48736482+bashar404@users.noreply.github.com>
2021-09-11 16:05:39 -0600

    addition (int)

--- Program.cs
+++ Program.cs
@@ -1,25 +1,16 @@
-using System;
+﻿using System;
 
 namespace ConsoleApp1
 {
-    class KLA
+    class addition
     {
         static void Main(string[] args)
         {
-            // Ctrl+k+c for comment
-            //Ctrl+K+u  for uncomment
-
-            Console.WriteLine("Enter your first Name:");
-            string fname = Console.ReadLine();
-            Console.WriteLine("Welcome" + " " + fname);  //concatenation
-
-            Console.WriteLine("Enter your last name: ");
-            string lname = Console.ReadLine();
-            Console.WriteLine(" Good Morning {0} {1}", fname, lname); //placeholder syntax
-
-
-            Console.WriteLine("Have a good day " + fname + " " + lname);
-
+            Console.WriteLine("Enter 2 no: ");
+            int num1 = int.Parse( Console.ReadLine() );
+            int num2 = int.Parse( Console.ReadLine() );
+            int sum = num1 + num2;
+            Console.WriteLine(" Addition is {0}" , sum );
             Console.ReadLine();
         }
     }
