91554e609dba84edab83c261389ba853272be712
Golam Dastoger Bashar <48736482+bashar404@users.noreply.github.com>
2021-09-11 11:38:29 -0600

    Add files via upload
    print name

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class KLA
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Welcome" + " " + name);
+            Console.WriteLine(" Good Morning {0}", name);
+            Console.ReadLine();
+        }
+    }
+}
