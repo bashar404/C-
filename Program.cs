35a94dd717683bd034896db124914f8c92b91322
Golam Dastoger Bashar <48736482+bashar404@users.noreply.github.com>
2021-09-13 10:27:43 -0600

    Parse-convert s to i

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Enter 2 no: ");
-            int num1 = int.Parse( Console.ReadLine() );
+            int num1 = int.Parse( Console.ReadLine() );  
             int num2 = int.Parse( Console.ReadLine() );
             int sum = num1 + num2;
             Console.WriteLine(" Addition is {0}" , sum );
